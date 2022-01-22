e78e3b2e7058da43fda6f8547858beeb21d2bc7e
Brett Smith <bsmith@mcad.edu>
2022-01-22 17:00:49 -0600

    Creature complete

--- week1/assign/snowcreature.js
+++ week1/assign/snowcreature.js
@@ -1,14 +1,16 @@
 let grid;
+let stroke1 = prompt('enter a basic color name in lowercase', 'pink')
+let stroke2 = prompt('enter a basic color name in lowercase', 'grey')
 function setup(){
     createCanvas(1000, 800);
     background('#ccc');
     grid = loadImage("image/100px_grid.png");
 }
 function draw() {
-    background(grid);
+    // background(grid);
     // snowman legs
     fill("f1f1f1");
-    stroke(100, 250, 100);
+    stroke(stroke1);
     strokeWeight(20);
     // left leg
     ellipse(350, 650, 200, 100);
@@ -19,7 +21,19 @@ function draw() {
     // head
     ellipse(500, 150, 200, 200);
     // hat brim
-    stroke(100,100,1800);
+    stroke(stroke2);
     strokeWeight(41);
     line(375,100,625,100);
+    // hatbody
+    quad(450, 50, 550, 50, 575, 100, 425, 100);
+    // eyes
+    stroke(0);
+    strokeWeight(50);
+    point(425,150);
+    point(575,150);
+    // mouth
+    noFill();
+    strokeWeight(15);
+    arc(500, 190, 80, 40, 10, HALF_PI);
+
 }
