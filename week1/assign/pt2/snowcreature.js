3b193a13dfdc320813aac7fdf3a3f4d07fe525dc
Brett Smith <bsmith@mcad.edu>
2022-01-23 12:14:50 -0600

    Created tree 1

--- week1/assign/pt2/snowcreature.js
+++ week1/assign/pt2/snowcreature.js
@@ -1,16 +1,16 @@
 let grid;
-let stroke1 = prompt('enter a basic color name in lowercase', 'pink')
-let stroke2 = prompt('enter a basic color name in lowercase', 'grey')
+// let stroke1 = prompt('enter a basic color name in lowercase', 'pink')
+// let stroke2 = prompt('enter a basic color name in lowercase', 'grey')
 function setup(){
     createCanvas(1000, 800);
     background('#ccc');
     grid = loadImage("image/100px_grid.png");
 }
 function draw() {
-    // background(grid);
+    background(grid);
     // snowman legs
     fill("f1f1f1");
-    stroke(stroke1);
+    // stroke(stroke1);
     strokeWeight(20);
     // left leg
     ellipse(350, 650, 200, 100);
@@ -21,7 +21,7 @@ function draw() {
     // head
     ellipse(500, 150, 200, 200);
     // hat brim
-    stroke(stroke2);
+    // stroke(stroke2);
     strokeWeight(41);
     line(375,100,625,100);
     // hatbody
@@ -35,5 +35,24 @@ function draw() {
     noFill();
     strokeWeight(15);
     arc(500, 190, 80, 40, 10, HALF_PI);
-
+    // treeTrunk
+    fill('#72531E');
+    strokeWeight(0)
+    quad(90, 440, 110, 440, 115, 550, 85, 550)
+    // treeBottom
+    fill('#1A4008');
+    strokeWeight(0);
+    triangle(100, 250, 40, 450, 160, 465);
+    // treeMidBottom
+    fill('#2B690D');
+    strokeWeight(0);
+    triangle(100, 150, 50, 330, 150, 350);
+    // treeTopMid
+    fill('#3B9212');
+    strokeWeight(0);
+    triangle(100, 100, 60, 255, 140, 247);
+    // treeTop
+    fill('#4CBB17');
+    strokeWeight(0);
+    triangle(100, 50, 70, 150, 130, 165);
 }
