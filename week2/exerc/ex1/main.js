880aeb4041afee51abb93758391bce65f8e5646d
Brett Smith <bsmith@mcad.edu>
2022-01-26 23:11:58 -0600

    Init movies

---
+++ week2/exerc/ex1/main.js
@@ -0,0 +1,5 @@
+const userMovie = window.prompt("What is a bad movie that you love?");
+const myMovie = ['Dracula 1979.A.D', 'Prometheus', 'FaceOff'];
+ourMovie = myMovie.concat(userMovie);
+
+console.log (ourMovie.toString());
