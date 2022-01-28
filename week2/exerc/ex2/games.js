fadb424b57f40f0eb7c27904e6e6f831cb1bd141
Brett Smith <bsmith@mcad.edu>
2022-01-27 22:00:39 -0600

    Init game array

---
+++ week2/exerc/ex2/games.js
@@ -0,0 +1,39 @@
+// const favGames = ['Settlers', 'Wingspan', 'Cave Evil', 'Talisman', 'Lords of Waterdeep']
+const yourGame = window.prompt('There are 5 games in my collection. pick a number to learn about a game');
+
+const gameOne = {
+    name: 'Settlers of Catan',
+    theme: 'Agrarian Monopoly',
+    numberOfplayers: '2-4',
+    funRating: 1,
+    agravationRating: 3
+};
+const gameTwo = {
+    name: 'Wingspan',
+    theme: 'Bird Watching',
+    numberOfplayers: '1-5',
+    funRating: 1,
+    agravationRating: 2
+};
+const gameThree = {
+    name: 'Cave Evil',
+    theme: 'Necromantic Monster Breeding',
+    numberOfplayers: '2-4',
+    funRating:2,
+    agravationRating:2
+};
+const gameFour = {
+    name: 'Talisman',
+    theme: 'Fantasy Wanderings',
+    numberOfplayers: '1-8',
+    funRating: 2,
+    agravationRating: 3
+};
+const gameFive = {
+    name: 'Lords of Waterdeep',
+    theme: 'Fantasy Cang Managment',
+    numberOfplayers: '1-5',
+    funRating: 1,
+    agravationRating: 2
+};
+
