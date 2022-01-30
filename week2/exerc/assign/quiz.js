1fbb78531f33f87a6533e8f059ae356a69676333
Brett Smith <bsmith@mcad.edu>
2022-01-30 12:38:38 -0600

    Init quiz 1

---
+++ week2/exerc/assign/quiz.js
@@ -0,0 +1,44 @@
+const questOne = {
+    question: 'What did the egg say to the frying pan?',
+    answer: 'You crack me up.'
+};
+const questTwo = {
+    question: 'How do you get an alien baby to sleep?',
+    answer: 'You rocket.'
+};
+const questThree = {
+    question: 'What kind of tea is sometimes hard to swallow?',
+    answer: 'Reality.'
+};
+const questFour = {
+    question: 'What is the definition of a farmer?',
+    answer: 'Someone is good in their field.'
+};
+const questFive = {
+    question: "Why dont lobster's share?",
+    answer: "Because they are shellfish."
+};
+const questFive = {
+    question: "Who took the frog's car?",
+    answer: "It was toad."
+};
+const questSix = {
+    question: "How do you communicate with a fish?",
+    answer: "You drop it a line."
+};
+const questSeven = {
+    question: "What kind of horses go out after dusk?",
+    answer: "Nightmares!"
+};
+const questEight = {
+    question: "Why did the banana go to the doctor?",
+    answer: "He wasn't peeling well!"
+};
+const questNine = {
+    question: "What did 0 say to 8?",
+    answer: "Nice belt!"
+};
+const questNine = {
+    question: "Whats the vest way to carve wood?",
+    answer: "Whittle by whittle."
+};
