6349cdaa6026a442f7af89f5a426d04dd971d577
Brett Smith <bsmith@mcad.edu>
2022-02-03 17:33:53 -0600

    Tile working
    Having issues making p5.JS pull from the web. added local files

--- week3/exerc/ex1/tile.js
+++ week3/exerc/ex1/tile.js
@@ -6,12 +6,19 @@ function createTile() {
     fill('orange');
     rect(0, 0, 200, 200);
     stroke('pink');
-    strokeWeight(5);
+    strokeWeight(10);
     line(0, 0, 200, 200);
     line(200, 0, 0, 200);
     noStroke();
-    fill('black');
-    ellipse(125, 50, 30, 60);
+    fill('yellow');
+    circle(100, 40, 30);
+    circle(160, 100, 30);
+    circle(100, 160, 30);
+    circle(40, 100, 30);
+    fill('green');
+    circle(100, 100, 80);
+    fill('blue');
+    circle(100, 100, 60);
 }
 function draw() {
     createTile();
