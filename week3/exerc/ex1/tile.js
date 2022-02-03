1cc36e288958d37b8918ced6a7235428a90cdb8e
Brett Smith <bsmith@mcad.edu>
2022-02-02 22:17:40 -0600

    added lines and circles

--- week3/exerc/ex1/tile.js
+++ week3/exerc/ex1/tile.js
@@ -1,10 +1,17 @@
 function setup() {
-    createCanvas(200, 200,);
+    createCanvas(200, 200);
 }
 function createTile() {
     translate(0, 0);
     fill('orange');
     rect(0, 0, 200, 200);
+    stroke('pink');
+    strokeWeight(5);
+    line(0, 0, 200, 200);
+    line(200, 0, 0, 200);
+    noStroke();
+    fill('black');
+    ellipse(125, 50, 30, 60);
 }
 function draw() {
     createTile();
