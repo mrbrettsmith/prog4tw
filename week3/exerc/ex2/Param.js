f5e31068298d95096f2362ab3642cf29d05d4e02
Brett Smith <bsmith@mcad.edu>
2022-02-03 21:50:59 -0600

    Tile paramater finished

--- week3/exerc/ex2/Param.js
+++ week3/exerc/ex2/Param.js
@@ -1,22 +1,38 @@
 function setup() {
-    createCanvas(200, 200);
+    createCanvas(800, 800);
     noLoop();
 }
-function createTile() {
-    translate(0, 0);
-    fill('orange');
+function createTile(oraginX, oraginY, background, frontline, backline) {
+    translate(oraginX, oraginY);
+    noStroke()
+    fill(background);
     rect(0, 0, 200, 200);
     noFill();
     strokeWeight(20);
-    stroke('lightblue');
+    stroke(backline);
     line(0, 55, 140, 84);
     line(0, 144, 180, 182);
-    stroke('pink');
+    stroke(frontline);
     arc(100, 150, 50, 50, 96, 99, [PI]);
     arc(50, 50, 50, 50, 96, 99, [QUARTER_PI]);
     line(55, 25, 200, 55);
     line(105, 125, 200, 144);
 }
 function draw() {
-    createTile();
+    createTile(0,0, color(255,153,0),"lightblue","pink");
+    createTile(0,200, color(230,153,0),"pink","lightblue");
+    createTile(0,200,color(255,153,0),"lightblue","pink");
+    createTile(0,200,color(230,153,0),"pink","lightblue");
+    createTile(200,-600,color(230,153,0),"pink","lightblue");
+    createTile(0,200,color(255,153,0),"lightblue","pink");
+    createTile(0,200,color(230,153,0),"pink","lightblue");
+    createTile(0,200,color(255,153,0),"lightblue","pink");
+    createTile(200,-600,color(255,153,0),"lightblue","pink");
+    createTile(0,200,color(230,153,0),"pink","lightblue");
+    createTile(0,200,color(255,153,0),"lightblue","pink");
+    createTile(0,200,color(230,153,0),"pink","lightblue");
+    createTile(200,-600,color(230,153,0),"pink","lightblue");
+    createTile(0,200,color(255,153,0),"lightblue","pink");
+    createTile(0,200,color(230,153,0),"pink","lightblue");
+    createTile(0,200,color(255,153,0),"lightblue","pink");
 }
