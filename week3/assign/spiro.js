ac538ce1484f341b5f3b0f09806c5f969ce076e3
Brett Smith <bsmith@mcad.edu>
2022-02-06 15:39:26 -0600

    Commit Circle

--- week3/assign/spiro.js
+++ week3/assign/spiro.js
@@ -1,6 +1,14 @@
-function setUp() {
-    createCanvas(800, 800);
+function setup() {
+    createCanvas(800, 800)
     background(0);
-    anglemode(degrees);
+    angleMode(DEGREES);
 }
-  
+
+function draw() {
+    translate(300, 300);
+    rotate(5);
+    noFill();
+    stroke("red")
+    strokeWeight(5);
+    ellipse(150, 150, 100);
+}
