caf99c0f970d83d95a3570217e08cb57956160ce
Brett Smith <bsmith@mcad.edu>
2022-02-06 17:15:14 -0600

    Press and Release

--- week3/assign/spiro.js
+++ week3/assign/spiro.js
@@ -1,14 +1,29 @@
+let rotateBy = 5;
 function setup() {
     createCanvas(800, 800)
     background(0);
     angleMode(DEGREES);
 }
+function makeArm(rotateBy) {
+    let alt = Math.round(rotateBy / 360); //why does math.round work?
+    console.log(alt);
+    noFill();
+    stroke("red");
+    strokeWeight(5);
+    ellipse(150 + alt, 150 , 100);
+}
 
 function draw() {
     translate(300, 300);
-    rotate(5);
-    noFill();
-    stroke("red")
-    strokeWeight(5);
-    ellipse(150, 150, 100);
+    rotate(rotateBy);
+    makeArm(rotateBy);
+    rotateBy += 5;
+}
+
+function mousePressed() {
+    noLoop();
+  }
+function mouseReleased() {
+    loop();
+    stroke("blue");
 }
