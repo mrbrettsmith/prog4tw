d1038898724dd9152a03ff15bfe27864dec6ee32
Brett Smith <bsmith@mcad.edu>
2022-02-06 22:29:45 -0600

    added crazy effects

--- week3/assign/spiro.js
+++ week3/assign/spiro.js
@@ -4,7 +4,7 @@ let rotateBy = 5;
 function setup() {
     createCanvas(windowWidth, windowHeight);
     angleMode(DEGREES);
-    background(255, 0, 200);
+    background(255, 0, 200); //Initial Background
   }
   
   function windowResized() {
@@ -13,24 +13,26 @@ function setup() {
   }
 
 function makeArm(rotateBy) {
-    let alt = Math.round(rotateBy / 360); //why does math.round work?
+    let alt = Math.round(rotateBy / 25); //why does math.round work? 10 Determines how many cycles before alt is activated
     console.log(alt);
     noFill();
     stroke(randomColor);
     strokeWeight(5);
-    ellipse(150 + alt, 150 , 100);
+    // ellipse(100 + alt, 100 , 100);
+    ellipse(150 + alt, random(100, 250) , 100); // Crazy Wreath Effect!
 }
 
+
+
 function draw() {
-    translate(width /2, 300);
+    translate(width /2, height /2); //Center & Align Circle
     rotate(rotateBy);
     makeArm(rotateBy);
-    rotateBy += 5;
+    rotateBy += -5; //If negative it spirals in until 0, then back out?
 }
 function mousePressed() {
     noLoop();
-    randomColor = color(random(255),random(255),random(255));
-    let fallBack = -10;
+    randomColor = color(random(255),random(255),random(255)); //Stroke Randomizer
   }
 function mouseReleased() {
     loop();
