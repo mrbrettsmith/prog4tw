25fbe0fdb95b2771f698fad075b34da008738cdf
Brett Smith <bsmith@mcad.edu>
2022-02-06 23:30:22 -0600

    Final Funnelcake

--- week3/assign/spiro.js
+++ week3/assign/spiro.js
@@ -14,24 +14,17 @@ function makeArm(rotateBy) {
     let alt = Math.round(rotateBy / 25); //why does math.round work? 10 Determines how many cycles before alt is activated
     console.log(rotateBy);
     noFill();
-    stroke(randomColor);
+    stroke(randomColorOne);
     strokeWeight(5);
     // ellipse(100 + alt, 100 , 100);
     ellipse(150 + alt, random(60, 200) , random(60, 120)); // Crazy Wreath Effect!
-    // beginShape(TRIANGLES); //Sunburst
-    // vertex(60, 20);
-    // vertex(70, 75);
-    // vertex(80, 20 );
-    // endShape();
+    stroke(randomColorTwo);
+    beginShape(TRIANGLES); //Sunburst
+    vertex(60 + alt, 20 + alt);
+    vertex(70, 75);
+    vertex(80, 20);
+    endShape();
 }
-//      Trying to set a stop & reverse with no luck!
-// function bounceBack(rotateBy) {
-//     let x= rotateBy
-//     if(x > 2000){
-//         x= rotateBy * -1;
-//     }
-// }
-
 function draw() {
     translate(width /2, height /2); //Center & Align Circle
     rotate(rotateBy);
@@ -41,8 +34,19 @@ function draw() {
 }
 function mousePressed() {
     noLoop();
-    randomColor = color(random(255),random(255),random(255)); //Stroke Randomizer
+    randomColorOne = color(random(255),random(255),random(255));
+    randomColorTwo = color(random(255)+5,random(255),random(255)); //Stroke Randomizer
   }
 function mouseReleased() {
     loop();
 }
+
+// WIP
+
+//      Trying to set a stop & reverse with no luck!
+// function bounceBack(rotateBy) {
+//     let x= rotateBy
+//     if(x > 2000){
+//         x= rotateBy * -1;
+//     }
+// }
