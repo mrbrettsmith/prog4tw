cbef58bf077a115c112915d64b0baaf6dafdb134
Brett Smith <bsmith@mcad.edu>
2022-02-10 23:09:41 -0600

    Update ex2.js

--- week4/exerc/ex2/ex2.js
+++ week4/exerc/ex2/ex2.js
@@ -1,5 +1,6 @@
-let = rectX = 0;
+let rectX = 0;
 let rectWitdth = 50
+let clickCount = 0;
 function setup() {
     createCanvas(500, 500);
     background('orange');
@@ -13,7 +14,8 @@ function draw(){
 function mousePressed() {
     // if ((mouseX >= 225 && mouseX <= 275) && (mouseY <=225 && mouseY <= 275))
     if((mouseX >= rectX && mouseX <= rectX + rectWitdth) && (mouseY >= 0 && mouseY <= 50))
-    console.log('gotcha!')
+    clickCount;
+    console.log('gotcha!', clickCount)
 }
 
 
