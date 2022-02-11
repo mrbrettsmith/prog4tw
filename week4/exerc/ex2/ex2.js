f5a1acbe19f7cf1c21a83afae83e721a234df525
Brett Smith <bsmith@mcad.edu>
2022-02-10 22:58:18 -0600

    Made rect shape

--- week4/exerc/ex2/ex2.js
+++ week4/exerc/ex2/ex2.js
@@ -2,8 +2,19 @@ function setup() {
     createCanvas(500, 500);
     background('orange');
 }
+function draw(){
+    background('orange');
+    drawShape();
+}
 
-function bigCirc(){
-    fill('rose');
-    ellipse(250, 250, 50, 50);
+function mousePressed() {
+    // if ((mouseX >= 225 && mouseX <= 275) && (mouseY <=225 && mouseY <= 275))
+    if((mouseX >= 0 && mouseX <= 50) && (mouseY >= 0 && mouseY <= 50))
+    console.log('gotcha!')
+}
+
+function drawShape() {
+    fill('yellow');
+    rect(0, 0, 50, 50,);
+    // ellipse(250, 250, 50, 50);
 }
