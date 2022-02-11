616a5bde21380b6448afed4949a09b52cadd669f
Brett Smith <bsmith@mcad.edu>
2022-02-10 21:20:57 -0600

    added JS code

---
+++ week4/exerc/ex1/main.js
@@ -0,0 +1,7 @@
+// select empty div from HTML (document.) and assign it a variable
+const myEmptyDiv = document.querySelector("myEmptyDiv");
+
+// take variable and add H1 inside of it
+const heading = document.createElement("h1");
+heading.innerHTML = "Llama"
+myEmptyDiv.appendChild(heading);
