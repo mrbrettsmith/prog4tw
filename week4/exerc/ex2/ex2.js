aee922a0ae2e320cf119bff75991dfde0fe8007e
Brett Smith <bsmith@mcad.edu>
2022-02-10 23:28:45 -0600

    Yay! random is working!

--- week4/exerc/ex2/ex2.js
+++ week4/exerc/ex2/ex2.js
@@ -1,9 +1,12 @@
 let rectX = 0;
-let rectWitdth = 50
+let rectY;
+const rectWitdth = 50;
+const rectHeight = 50;
 let clickCount = 0;
 function setup() {
     createCanvas(500, 500);
     background('orange');
+    rectY = random(0, height - rectHeight);
 }
 function draw(){
     background('orange');
@@ -17,7 +20,7 @@ function draw(){
 
 function mousePressed() {
     // if ((mouseX >= 225 && mouseX <= 275) && (mouseY <=225 && mouseY <= 275))
-    if((mouseX >= rectX && mouseX <= rectX + rectWitdth) && (mouseY >= 0 && mouseY <= 50))
+    if((mouseX >= rectX && mouseX <= rectX + rectWitdth) && (mouseY >= rectY && mouseY <= rectY + rectHeight))
     clickCount++;
     console.log('gotcha!', clickCount)
 }
@@ -26,7 +29,7 @@ function mousePressed() {
 
 function drawShape() {
     fill('yellow');
-    rect(rectX, 0, 50, rectWitdth);
+    rect(rectX, rectY, rectWitdth, rectHeight);
     
     // ellipse(250, 250, 50, 50);
 }
