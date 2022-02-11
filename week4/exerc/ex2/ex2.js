3a0368230108a1235051edf7e38bff0a25e4ef5f
Brett Smith <bsmith@mcad.edu>
2022-02-10 23:06:53 -0600

    rectangle is moving and has hitbox

--- week4/exerc/ex2/ex2.js
+++ week4/exerc/ex2/ex2.js
@@ -1,3 +1,5 @@
+let = rectX = 0;
+let rectWitdth = 50
 function setup() {
     createCanvas(500, 500);
     background('orange');
@@ -5,16 +7,20 @@ function setup() {
 function draw(){
     background('orange');
     drawShape();
+    rectX += 2;
 }
 
 function mousePressed() {
     // if ((mouseX >= 225 && mouseX <= 275) && (mouseY <=225 && mouseY <= 275))
-    if((mouseX >= 0 && mouseX <= 50) && (mouseY >= 0 && mouseY <= 50))
+    if((mouseX >= rectX && mouseX <= rectX + rectWitdth) && (mouseY >= 0 && mouseY <= 50))
     console.log('gotcha!')
 }
 
+
+
 function drawShape() {
     fill('yellow');
-    rect(0, 0, 50, 50,);
+    rect(rectX, 0, 50, rectWitdth);
+    
     // ellipse(250, 250, 50, 50);
 }
