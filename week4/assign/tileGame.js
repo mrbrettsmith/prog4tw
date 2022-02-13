4eba1281ef7ad3bb186aab86e481d7b48639a8ac
Brett Smith <bsmith@mcad.edu>
2022-02-13 15:53:46 -0600

    Trying to place cardback

--- week4/assign/tileGame.js
+++ week4/assign/tileGame.js
@@ -2,43 +2,56 @@
 function setup() {
     createCanvas(1000, 1000);
     background('#A19FD4FF');
+    // function preload() {
+    //     img = loadImage('image/gametile.png');
+    //   }
 }
-
-function tileFrame(originX,originY,background) {
+function tileFrame(originX,originY,background,) {
     translate(originX,originY);
     noStroke();
-    fill(background)
+    fill(background);
     rect(0, 0, 100, 100);
 }
-// // Color Changer
+
+l
+
+// // Color Changer - Unsure how alpha channel works
 // let letterGround = color(cardBackColor, 0);
-// let cardBackColor = color('#6FD46CFF', [alpha]);
+// let cardBackColor = ('#6FD46CFF', [100]);
+// let texto= ('#6FD46CFF');
+
+
+// function tileBack() {
+//     back = loadImage("image/Gametile.png");
+//     image(back, 60, 60);
+// }
 
 function draw() {
     tileFrame(60,60,'#BDD44EFF');
-    tileFrame(0,140,'#BDD44EFF');
-    tileFrame(0,140,'#BDD44EFF');
-    tileFrame(0,140,'#BDD44EFF');
-    tileFrame(0,140,'#BDD44EFF');
-    tileFrame(140,-560,'#6FD46CFF');
-    tileFrame(0,140,'#6FD46CFF');
-    tileFrame(0,140,'#6FD46CFF');
-    tileFrame(0,140,'#6FD46CFF');
-    tileFrame(0,140,'#BDD44EFF');
-    tileFrame(140,-560,'#6FD46CFF');
-    tileFrame(0,140,'#6FD46CFF');
-    tileFrame(0,140,'#6FD46CFF');
-    tileFrame(0,140,'#6FD46CFF');
-    tileFrame(0,140,'#BDD44EFF');
-    tileFrame(140,-560,'#6FD46CFF');
-    tileFrame(0,140,'#6FD46CFF');
-    tileFrame(0,140,'#6FD46CFF');
-    tileFrame(0,140,'#6FD46CFF');
-    tileFrame(0,140,'#BDD44EFF');
-    tileFrame(140,-560,'#6FD46CFF');
-    tileFrame(0,140,'#6FD46CFF');
-    tileFrame(0,140,'#6FD46CFF');
-    tileFrame(0,140,'#6FD46CFF');
-    tileFrame(0,140,'#BDD44EFF');
-    
+    loadImage('image/gametile.png', img => {
+        image(img,0, 0);});
+    // tileFrame(0,140,'#BDD44EFF');
+    // tileFrame(0,140,'#BDD44EFF');
+    // tileFrame(0,140,'#BDD44EFF');
+    // tileFrame(0,140,'#BDD44EFF');
+    // tileFrame(140,-560,'#6FD46CFF');
+    // tileFrame(0,140,'#6FD46CFF');
+    // tileFrame(0,140,'#6FD46CFF');
+    // tileFrame(0,140,'#6FD46CFF');
+    // tileFrame(0,140,'#BDD44EFF');
+    // tileFrame(140,-560,'#6FD46CFF');
+    // tileFrame(0,140,'#6FD46CFF');
+    // tileFrame(0,140,'#6FD46CFF');
+    // tileFrame(0,140,'#6FD46CFF');
+    // tileFrame(0,140,'#BDD44EFF');
+    // tileFrame(140,-560,'#6FD46CFF');
+    // tileFrame(0,140,'#6FD46CFF');
+    // tileFrame(0,140,'#6FD46CFF');
+    // tileFrame(0,140,'#6FD46CFF');
+    // tileFrame(0,140,'#BDD44EFF');
+    // tileFrame(140,-560,'#6FD46CFF');
+    // tileFrame(0,140,'#6FD46CFF');
+    // tileFrame(0,140,'#6FD46CFF');
+    // tileFrame(0,140,'#6FD46CFF');
+    // tileFrame(0,140,'#BDD44EFF');
 }
