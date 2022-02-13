a166e34b19b119a51c59343354686d9966c43852
Brett Smith <bsmith@mcad.edu>
2022-02-13 11:48:12 -0600

    Grid finished

--- week4/assign/tileGame.js
+++ week4/assign/tileGame.js
@@ -8,15 +8,37 @@ function tileFrame(originX,originY,background) {
     translate(originX,originY);
     noStroke();
     fill(background)
-    rect(0, 0, 125, 125);
-    
+    rect(0, 0, 100, 100);
 }
-
-let letterGround = color(cardBackColor, 0);
-
-let cardBackColor = color('#6FD46CFF', [alpha]);
+// // Color Changer
+// let letterGround = color(cardBackColor, 0);
+// let cardBackColor = color('#6FD46CFF', [alpha]);
 
 function draw() {
-    tileFrame(75,75,'#6FD46CFF');
-
+    tileFrame(60,60,'#BDD44EFF');
+    tileFrame(0,140,'#BDD44EFF');
+    tileFrame(0,140,'#BDD44EFF');
+    tileFrame(0,140,'#BDD44EFF');
+    tileFrame(0,140,'#BDD44EFF');
+    tileFrame(140,-560,'#6FD46CFF');
+    tileFrame(0,140,'#6FD46CFF');
+    tileFrame(0,140,'#6FD46CFF');
+    tileFrame(0,140,'#6FD46CFF');
+    tileFrame(0,140,'#BDD44EFF');
+    tileFrame(140,-560,'#6FD46CFF');
+    tileFrame(0,140,'#6FD46CFF');
+    tileFrame(0,140,'#6FD46CFF');
+    tileFrame(0,140,'#6FD46CFF');
+    tileFrame(0,140,'#BDD44EFF');
+    tileFrame(140,-560,'#6FD46CFF');
+    tileFrame(0,140,'#6FD46CFF');
+    tileFrame(0,140,'#6FD46CFF');
+    tileFrame(0,140,'#6FD46CFF');
+    tileFrame(0,140,'#BDD44EFF');
+    tileFrame(140,-560,'#6FD46CFF');
+    tileFrame(0,140,'#6FD46CFF');
+    tileFrame(0,140,'#6FD46CFF');
+    tileFrame(0,140,'#6FD46CFF');
+    tileFrame(0,140,'#BDD44EFF');
+    
 }
