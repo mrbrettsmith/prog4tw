5cafcf7ecc36e089b8853bc9fe091fbcb2c65dd3
Brett Smith <bsmith@mcad.edu>
2022-02-17 23:23:01 -0600

    exc 1 init

---
+++ week5/exerc/ex1/w5ex1.js
@@ -0,0 +1,16 @@
+let greekMythicalCharacters = ["Athena", "Zeus", "Hermes", "Hercules", "Hera", "that one guy"];
+
+greekMythicalCharacters.pop();
+greekMythicalCharacters.push("Hades");
+
+console.log (greekMythicalCharacters);
+
+
+
+// let myDogs = ['lucky', 'Lucy', 'Limda', 'Lacey', 'Carl'];
+
+// for (const d of myDogs) { console.log("My dog, " + (d), "is a really good pupper.");
+// }
+
+// // let dogTag = myDogs;
+
