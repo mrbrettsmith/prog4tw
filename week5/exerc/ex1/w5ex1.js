fae7a6d5d5f054211f67a606ea9bfee19281d4d8
Brett Smith <bsmith@mcad.edu>
2022-02-18 00:27:37 -0600

    added key map

--- week5/exerc/ex1/w5ex1.js
+++ week5/exerc/ex1/w5ex1.js
@@ -1,5 +1,6 @@
 let blockX = 0;
 let blockY = 0;
+let BlockColor = "pink";
 let drawTimer;
 let distance = 10;
 let speed = 30;
@@ -13,18 +14,27 @@ function setup() {
 function drawBlock(x,y, color) {
     fill(color || "pink");
     rect(x, y, 50, 50);
-    
+}
+function keyTyped() {
+    let keyToNumber = number(key);
+     if (isNaN(keyToNumber)){
+        return;}
+    keyToNumber = map(keyToNumber, 1, 9, 1, 255);
+    BlockColor =keyToNumber;
+    console.log('youpressed', keyToNumber);
+   
+
 }
 
 drawTimer = window.setInterval(() => {
         if (blockX -50 <= 400) {
-        drawBlock(blockX,blockY,"red"); 
+        drawBlock(blockX, blockY, BlockColor); 
         blockX += distance;
     } else {
         blockX = 0;
         blockY += 50;
     }
-    if (blockX  > width && blockY  > height) {
+    if (blockX - 50 > width && blockY - 50 > height) {
         window.clearInterval(drawTimer);
         alert('collison!');
     }
