d13a95b52f6f9881f3bb9d62241ce53ede5d8f7d
Brett Smith <bsmith@mcad.edu>
2022-02-18 18:24:53 -0600

    Added constructor and functions
    still not drawing?

--- week5/exerc/ex2/w5ex2.js
+++ week5/exerc/ex2/w5ex2.js
@@ -1,4 +1,32 @@
+let bubble;
+
 function setup() {
-    background('red');
     createCanvas(600, 600);
+    bubble = new Bubble();
+    print(bubble.x,bubble.y)
+}
+
+function draw() {
+    background('red');
+    bubble.move();
+    bubble.show;
+}
+
+class Bubble {
+    constructor() {
+        this.x = 100;
+        this.y = 100;
+        this.width = 100;
+        this.height = 80;
+    }
+    move() {
+        this.x = this.x + random(-5, 10);
+        this.y = this.y + random(-5, 10);
+    }
+    show() {
+        stroke("pink");
+        strokeWeight(7);
+        noFill();
+        ellipse(this.x, this.y, this.width, this.height);
+    }
 }
