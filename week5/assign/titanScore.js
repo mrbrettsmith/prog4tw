e94f641f6cbca093e4952093707c3163e8179a65
Brett Smith <bsmith@mcad.edu>
2022-02-20 15:56:51 -0600

    Card facing correct
    === in flip protocol instead of =
    
    Timer still broken

--- week5/assign/titanScore.js
+++ week5/assign/titanScore.js
@@ -1,21 +1,21 @@
-let myCard;
+
 const DOWN = 'down';
 const UP = 'up';
 let startingX = 340;
 let startingY = 60; 
 let cards = [];
+
 //creating the scoring & status of game
 const gameState = {
     totalPairs: 8,
     flippedCards: [],
     numMatched: 0,
     attempts: 0,
-    waiting: false
-}
-
+    waiting: false,
+};
+// Image Info
 let cardFaceArray = []
 let cardBack;
-
 function preload() {
     cardBack = loadImage('image/dice.png');
     cardFaceArray = [
@@ -29,10 +29,10 @@ function preload() {
         loadImage('image/wheat.png')
     ]
 }
-
+// gameboard Set Up
 function setup() {
     createCanvas(1000, 1000);
-    // background('#A19FD4FF')
+    background('#A19FD4FF') // Needs to be here?
     let selectedFaces = [];
     for (let z = 0; z < 8; z++) {
         const randomIdx = floor(random(cardFaceArray.length))
@@ -42,40 +42,38 @@ function setup() {
         //remove used cardface
         cardFaceArray.splice(randomIdx, 1);
     }
-    // myCard = new Card(); // can I get rid of this?
     selectedFaces = shuffleArray(selectedFaces);
     for (let c = 0; c < 4; c++) {
         for (let r = 0; r < 4; r++) {
-            const faceImage =selectedFaces.pop();
-            cards.push(new Card(startingX, startingY, faceImage));
+            const cardFaceImg = selectedFaces.pop();
+            cards.push(new Card(startingX, startingY, cardFaceImg));
             startingX += 140;
         }
         startingY += 140;
         startingX = 340;
     }
 }
-// now background is in front of cards?
 function draw() {
-    background('#A19FD4FF'); // weird 
+    // background('#A19FD4FF'); // now background is in front of cards?
     if (gameState.numMatched === gameState.totalPairs) {
         fill('orange');
         textSize(100);
-        text('Winner!', 400, 400)
+        text('Winner!', 400, 400);
         noloop();
     }
-    for (let a = 0; a > cards.length; a++) {
-        if(!cards[a].isMatch) {
-            cards[a].face = DOWN;
+    for (let k = 0; k > cards.length; k++) {
+        if (!cards[k].isMatch) {
+            cards[k].face = DOWN;
         }
-        cards[a].show();
+        cards[k].show();
     }
     noloop();
     gameState.flippedCards.length = 0;
     gameState.waiting = false;
-    fill('red');
+    fill('orange');
     textSize(36);
-    text('attempts ' + gameState.attempts, 100 ,300);
-    text('matches ' + gameState.numMatched, 100, 400)
+    text('attempts ' + gameState.attempts, 340 ,300);
+    text('matches ' + gameState.numMatched, 340, 350);
 }
 
 
@@ -86,7 +84,7 @@ function mousePressed() {
     for (let m = 0; m < cards.length; m++) {
         // Check to see how many have been flipped, then trigger flip
         if (gameState.flippedCards.length < 2 && cards[m].didHit(mouseX, mouseY)) {
-            console.log('card is', cards[m]);
+            // console.log('card is', cards[m]);
             gameState.flippedCards.push(cards[m]);
         }   
     }
@@ -105,7 +103,7 @@ function mousePressed() {
             const loopTimeout = window.setTimeout(() => {
                 loop();
                 window.clearTimeout(loopTimeout);
-            }, 1000)
+            }, 1000);
         }
     }
 }
@@ -116,18 +114,21 @@ class Card {
         this.y = y;
         this.width = 100;
         this.height = 100;
+        this.face = DOWN;
         this.cardFaceImg = cardFaceImg;
         this.isMatch = false;
         this.show();
-        this.face = DOWN;
+        // this.face = DOWN;
     }
-    show() { // whyare my upp&down reversed?
+    show() {
         noStroke();
-        if (this.face === DOWN || this.isMatch) {
+        // face up = dice
+        if (this.face === UP || this.isMatch) {
             fill('#72BAD4FF'); 
             rect(this.x, this.y, this.width, this.height,)
             image(this.cardFaceImg, this.x, this.y);
         } else {
+        // face down = game sprite
             fill('#6FD46CFF'); 
             rect(this.x, this.y, this.width, this.height,);
             image(cardBack, this.x, this.y);
@@ -144,7 +145,7 @@ class Card {
     }
     flip () {
         if (this.face === DOWN) {
-            this.face === UP;
+            this.face = UP;
         } else {
             this.face = DOWN;
         }
