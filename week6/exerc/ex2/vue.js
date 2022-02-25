b11fe0d02e509418aeb472998a2e74a1998a5f76
Brett Smith <bsmith@mcad.edu>
2022-02-24 22:50:15 -0600

    Init Vue test
    Copied from plunkr

---
+++ week6/exerc/ex2/vue.js
@@ -0,0 +1,19 @@
+new Vue({
+    el: "#myApp",
+    data: {
+      message: "and that is what they say!",
+      time: "morning",
+      say: "words",
+      author:"Opal",
+  
+  
+  
+      // I am breaking something here:
+        // parentMessage: 'When cats wake up in the morning they always say...',
+      //   items: [
+      //      {message: 'meow'},
+      //      {message: 'meow'},
+      //      {message: 'meow'}]
+        // }
+      }
+  });
