86b9f0a88b9c1c33d2c02fbf0367bf8f20e1526e
Brett Smith <bsmith@mcad.edu>
2022-03-03 22:13:39 -0600

    finished!

--- week7/exerc/ex1/main.js
+++ week7/exerc/ex1/main.js
@@ -1,19 +1,17 @@
 Vue.createApp({
     data(){
         return {
+            truth: {
+                color: "purple",
+            },
+            thisIs: "false",
+
             title: "Llama and Alpaca",
             llamaSite: "https://en.wikipedia.org/wiki/Llama",
             alpacaSite: "https://en.wikipedia.org/wiki/Alpaca"
         }
     }
 }).mount("#app")
-
-
- // truth: {
-        //     color: "purple",
-
-        // },
-        // thisIs: "false",
 
 
 
