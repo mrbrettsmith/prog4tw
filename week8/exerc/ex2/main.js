d23a42ce22cbf23d16ae0f5f92eb8410c68bf6b3
Brett Smith <bsmith@mcad.edu>
2022-03-10 23:13:54 -0600

    Init and complete calculator

---
+++ week8/exerc/ex2/main.js
@@ -0,0 +1,40 @@
+let vm = Vue.createApp({
+    data() {
+        return {
+            firstNum: null,
+            secondNum: null,
+            operation: 'add',
+            total: null
+        }
+    },
+    methods: {
+        calculate: (num1, num2) => {
+            switch(vm.operation) {
+                case 'add':
+                    vm.total = num1 + num2;
+                    break;
+                case 'subtract':
+                    vm.total = num1 - num2;
+                    break;
+                case 'multiply':
+                    vm.total = num1 * num2;
+                    break;  
+                case 'divide':
+                    vm.total = num1 / num2;
+                    break; 
+                default:
+                    window.alert('unexpected operator :(')
+                    break;
+            }
+        }
+    },
+}).mount("#app")
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
