9036dca6bb5ea1c7fd3ebae573830e8c98dcae75
Brett Smith <bsmith@mcad.edu>
2022-03-24 22:56:03 -0500

    Loop is working

--- week9/exerc/ex2/week9demo/src/App.vue
+++ week9/exerc/ex2/week9demo/src/App.vue
@@ -1,11 +1,41 @@
 <script setup>
-
+const camelids = [
+  {
+    commonName: "Lama",
+    binomialName: "Lama glama",
+    order: "Artiodactyla",
+    family: "Camelidae",
+    genus: "Lama",
+    species: "L. glama",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Llama_lying_down.jpg/220px-Llama_lying_down.jpg"
+  },
+  {
+    commonName: "Alpaca",
+    binomialName: "Vicugna pacos",
+    order: "Artiodactyla",
+    family: "Camelidae",
+    genus: "Vicugna",
+    species: "V. pacos",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Alpaca_%2831562329701%29.jpg/440px-Alpaca_%2831562329701%29.jpg"
+  },
+]
 </script>
 
 <template>
+<!-- html here -->
+<h1>Camailid Componant Page</h1>
+<div v-for="beast in camelids" v-bind:key="beast.binomialName">
+  <h3>Common Name</h3>
+  <p>Binomial Name</p>
+  <p>Order</p>
+  <p>Family</p>
+  <p>Genus</p>
+  <p>Species</p>
+  <img src="HOLD" alt="commonName">
+</div>
 
 </template>
 
 <style>
-
+/* styyyles  */
 </style>
