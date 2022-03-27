73e7d650cec3c56992fdc58af9d23cb546f78e7a
Brett Smith <bsmith@mcad.edu>
2022-03-27 12:25:50 -0500

    Prettiered everything HTML

--- week9/assign/src/App.vue
+++ week9/assign/src/App.vue
@@ -18,18 +18,25 @@
         <!-- <th>Image</th> -->
       </thead>
       <tbody>
-        <tr v-for="(rock, idx) in rocks" v-bind:key="idx"
+        <tr
+          v-for="(rock, idx) in rocks"
+          v-bind:key="idx"
           v-bind:class="{
             gradTwo: idx % 2 !== 0,
             gradOne: idx % 2 === 0,
             lava: rock.name.toLowerCase().includes('lava'),
-        }">
+          }"
+        >
           <td>{{rock.name}}</td>
           <td>{{rock.place}}</td>
           <td>{{rock.who}}</td>
           <td>{{rock.dimensions}}</td>
           <td>{{rock.description}}</td>
-          <td><button type="button" v-on:click="deleteItem(rock)">Remove Rock</button></td>
+          <td>
+            <button type="button" v-on:click="deleteItem(rock)">
+              Remove Rock
+            </button>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -37,31 +44,56 @@
       <fieldset> 
         <legend>Add a new rock to the database</legend>
         <div class="form-container">
-        <div>
-          <label for="name">Name</label>
-          <input id="name " type="text" v-model="newRockObj.name"
+          <div>
+            <label for="name">Name</label>
+            <input
+              id="name "
+              type="text"
+              v-model="newRockObj.name"
               placeholder="name the rock">
+          </div>
+          <div>
+            <label for="place">Place</label>
+            <input
+              id="place "
+              type="text"
+              v-model="newRockObj.place"
+              placeholder="where it was found"
+            />
+          </div>
+          <div>
+            <label for="who">Who found it?</label>
+            <input
+              id="who "
+              type="text"
+              v-model="newRockObj.who"
+              placeholder="who gifted the rock"
+            />
+          </div>
+          <div>
+            <label for="dimensions">Dimensions</label>
+            <input
+              id="dimensions"
+              type="text"
+              v-model="newRockObj.dimensions"
+              placeholder="w, h, d, in inches"
+            />
+          </div>
+          <div class="new-rock-description">
+            <label for="description">Description</label>
+            <!-- <input id="description "type="text" v-model="newRockObj.description" placeholder="describe characteristics"> -->
+            <textarea
+              id="description"
+              rows="1"
+              cols="30"
+              type="text"
+              v-model="newRockObj.description"
+              placeholder="describe characteristics"
+            >
+            </textarea>
+          </div>
+          <button type="submit" class="new-rock-button">Submit Rock</button>
         </div>
-        <div>
-          <label for="place">Place</label>
-          <input id="place " type="text" v-model="newRockObj.place"
-                    placeholder="where it was found">
-        </div>
-        <div>
-          <label for="who">Who found it?</label>
-          <input id="who " type="text" v-model="newRockObj.who" placeholder="who gifted the rock">
-        </div>
-        <div>
-          <label for="dimensions">Dimensions</label>
-          <input id="dimensions" type="text" v-model="newRockObj.dimensions" placeholder="w, h, d, in inches">
-        </div>
-        <div class="new-rock-description">
-          <label for="description">Description</label>
-          <!-- <input id="description "type="text" v-model="newRockObj.description" placeholder="describe characteristics"> -->
-          <textarea id="description" rows="1" cols="30" type="text" v-model="newRockObj.description" placeholder="describe characteristics"> </textarea>
-        </div>
-        <button type="submit" class="new-rock-button"> Submit Rock</button>
-      </div>
       </fieldset>
     </form>
   </div>
