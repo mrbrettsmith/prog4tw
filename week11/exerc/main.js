9728568953be23e3453ea313d267dff67fab4154
Brett Smith <bsmith@mcad.edu>
2022-04-08 00:04:11 -0500

    I cant tell if p5JS is working!

--- week11/exerc/main.js
+++ week11/exerc/main.js
@@ -4,27 +4,20 @@ let grow = [];
 function setup() {
     createCanvas(600, windowHeight, WEBGL);
     background(100, random(255), 255);
-    
-  }
+    grow = new Leaf();
+    }
 
-
-
-
-  function draw() {
+function draw() {
     for (let l = 0; l < 7; l++) {
-        grow = new Leaf();
-        // grow[l].bud();
         grow[l].bloom();
+       
     }
   }
-
 
 class Leaf{
     constructor() {
         this.x = 100
         this.y = 100
-        this.width = 100;
-        this.height = 80;
     }
 
     // bud() {
@@ -33,10 +26,11 @@ class Leaf{
     // }
 
     bloom() {
-        stroke("pink");
-        strokeWeight(7);
-        noFill();
-        ellipse(this.x, this.y, this.width, this.height);
+        triangle(30, 75, 58, 20, 86, 75);
+        // stroke("pink");
+        // strokeWeight(7);
+        // noFill();
+        // triangle(this.x -25, this.y, this.x +25, this.y, this.x + random(-25, +25), this.y +100);
     }
 }
  
