48e9afc70d71a8eed74dca7638c606d4cc2b89bb
Brett Smith <bsmith@mcad.edu>
2022-04-07 23:40:33 -0500

    Confusing myself

--- week11/exerc/main.js
+++ week11/exerc/main.js
@@ -1,25 +1,42 @@
 
-let leafo = [];
-
-
+let grow = [];
 
 function setup() {
-    createCanvas(600, windowHeight);
+    createCanvas(600, windowHeight, WEBGL);
     background(100, random(255), 255);
+    
   }
+
+
+
 
   function draw() {
     for (let l = 0; l < 7; l++) {
-        grow = leafo[l];
-        
+        grow = new Leaf();
+        // grow[l].bud();
+        grow[l].bloom();
     }
   }
+
+
 class Leaf{
-  leafo() {
-     stroke("grey");
-     strokeWeight(4);
-     noFill();
-     triangle(30, 75, 58, 20, mouseX, mouseY);
- }  
+    constructor() {
+        this.x = 100
+        this.y = 100
+        this.width = 100;
+        this.height = 80;
+    }
+
+    // bud() {
+    //     this.x = this.x + 75;
+    //     this.y = this.y + random(-5, 5);
+    // }
+
+    bloom() {
+        stroke("pink");
+        strokeWeight(7);
+        noFill();
+        ellipse(this.x, this.y, this.width, this.height);
+    }
 }
  
