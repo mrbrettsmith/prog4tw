1902d3c23a3dea6437a1011a99d71fc5fbb3a98a
Brett Smith <bsmith@mcad.edu>
2022-04-08 16:52:59 -0500

    good lord what am i doing

--- week11/exerc/main.js
+++ week11/exerc/main.js
@@ -1,34 +1,77 @@
+let makeLeaf = [];
+// let point1 = {
+//     a: mouseX, 
+//     b: mouseY
+// }
 
-let grow = [];
+// let point2 = 
+// let point3 =
 
 function setup() {
-    createCanvas(600, windowHeight, WEBGL);
+    createCanvas(windowWidth, windowHeight, WEBGL);
     background(100, random(255), 255);
-    grow = new Leaf();
+    makeLeaf = new leaf();
+    mousePressed 
     }
+
+function leaf() {
+    stroke("pink");
+    strokeWeight(3)
+    noFill()
+    triangle(mouseX, mouseY, 200, 100, 150,200)
+}
 
 function draw() {
-    for (let l = 0; l < 7; l++) {
-        grow[l].bloom();
-    }
-  }
-
-class Leaf{
-    constructor() {
-        this.x = 100
-        this.y = 100
-    }
-
-    // bud() {
-    //     this.x = this.x + 75;
-    //     this.y = this.y + random(-5, 5);
+    triangle(mouseX, mouseY, 200, 100, 150,200)
+    // for (let i = 0; i < 7; i++) {
+    //     makeLeaf[i];
     // }
-
-    bloom() {
-        stroke("green");
-        strokeWeight(3);
-        noFill();
-        triangle(this.x -25, this.y, this.x +25, this.y, this.x + random(-25, +25), this.y +100);
-    }
 }
- 
+
+function mousePressed() {
+    makeLeaf;
+}
+
+// class Grow {
+//     leaf() {
+//         stroke("pink");
+//         triangle(100, 100, 200, 100, 150,200)
+
+// }
+// }
+// let grow = [];
+
+
+
+// function setup() {
+//     createCanvas(windowWidth, windowHeight, WEBGL);
+//     background(100, random(255), 255);
+//     grow = new Leaf();
+//     }
+
+// function draw() {
+//     for (let l = 0; l < 7; l++) {
+//         grow[l].bloom();
+//     }
+//   }
+
+// class Leaf{
+//     constructor() {
+//         this.x = 100
+//         this.y = 100
+//     }
+
+//     bud() {
+//         this.x = this.x + 75;
+//         this.y = this.y + random(-5, 5);
+//     }
+
+//     bloom() {
+//         stroke("green");
+//         strokeWeight(3);
+//         noFill();
+
+
+//         triangle(this.x -25, this.y, this.x +25, this.y, this.x + random(-25, +25), this.y +100);
+//     }
+// }
