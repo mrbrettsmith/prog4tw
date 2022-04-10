bb1c1c7fdb002f0a2f6389caee478b59e1805880
Brett Smith <bsmith@mcad.edu>
2022-04-10 14:23:18 -0500

    Color shift working

--- week11/assign/main.js
+++ week11/assign/main.js
@@ -2,13 +2,29 @@
 
 function setup() {
     createCanvas(1000, 1000)
-    // var tail = createVector(60, 60);
-    // var head = createVector(70, 70);
-    shoot = new Star(tail, head);
-    for (let i = 0; i < 5; i++) {
-        shoot[i] = new Star(tail, head);
+    
+}
+
+function draw() {
+    
+    for (let i = 0; i < 10; i++) {
+        line(50 + (i * 3), 50, 300 + (i *6), 500);
+        stroke(100 + (i * 5), 150 + (i * 5), 100+ (i * 5));
     }
 }
+
+
+// I just dont get it! 
+
+// function setup() {
+//     createCanvas(1000, 1000)
+//     var tail = createVector(60, 60);
+//     var head = createVector(70, 70);
+//     shoot = new Star(tail, head);
+//     for (let i = 0; i < 5; i++) {
+//         shoot[i] = new Star(tail, head);
+//     }
+// }
 
 // function starMan( originX, originY) {
 //     translate(originX, originY);
@@ -17,20 +33,19 @@ function setup() {
 
 // }
 
-function Star(begin, end) {
-    this.begin = begin;
-    this.end = end;
-    this.show = function() {
-        stroke("red");
-        line(this.begin.x, this.begin.y, this.end.x, this.end.y);
-    }   
-}
+// function Star(begin, end) {
+//     this.show = function() {
+//         stroke("red");
+//         line(this.begin.x, this.begin.y, this.end.x, this.end.y);
+        
+//     }   
+// }
 
-function draw() {
-
-    for (let i = 0; i < 10 ; i++) {
-        shoot[i].show();        
-    }
+// function draw() {
+//     shift = 50;
+//     for (let i = 0; i < 10 ; i++) {
+//         Star(+ shift);    
+//     }
     // shoot.show();
     // for (let s = 0; s < 5; s++) {
     //     for (let s = 0; s < 5; s++) {
@@ -42,4 +57,4 @@ function draw() {
         
     // }
     // var fall = new starMan(width / 4, width / 2)
-}
+// }
