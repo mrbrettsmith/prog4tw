3537a9f8b1d9ce2577e6f4c4b2a8fe7708e370e7
Brett Smith <bsmith@mcad.edu>
2022-04-12 17:20:22 -0500

    yeargh!

--- week11/assign/sketchTwo.js
+++ week11/assign/sketchTwo.js
@@ -1,17 +1,32 @@
+let makeDot = [];
+
 function setup(){
-createCanvas(1000, 1000);
-r = 5;
-makeDot = new Dot(this.x, this.y, this.r);
+    createCanvas(1000, 1000);
+    r = 5;
+    // makeDot[d] = new Dot(this.x, this.y, this.r);
+    for (let d = 0; d < 49; d++) {
+        let x = 10 + 20 * d;
+        let y = 10 + 20 * d;
+        makeDot[d] = new Dot (x, y, r);
+       
+        
+        // makeDot[this.x, this.y, this.r].show();
+    };
 }
     
 function draw(){
-    
-    
-    for (let x = 0; x < 49; x++) {
-        for (let y = 0; y < 49; y++) {
-            makeDot[this.x, this.y, this.r];
-        }
+    for (let d = 0; d < makeDot.length; d++) {
+        
+        makeDot[d].show();
+        
     }
+
+    // for (let d = 0; d < 49; d++) {
+    //     makeDot[this.x, this.y, this.r].show();
+    //     // for (let y = 0; y < 49; y++) {
+    //     //     makeDot[this.x, this.y, this.r].show();
+    //     // }
+    // }
 }
 
 function mouseOver() {
@@ -24,12 +39,12 @@ function mouseOver() {
 
 class Dot {
     constructor(x,y){
-    this.x = x;
-    this.y = y;
-    this.r = r;
-}
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
     show() {
-    ellipse(this.x , this.y, this.r * 2);
+        ellipse(this.x , this.y, this.r * 2);
     }
 }
     // function swell() {
