6521ff39b0c6f49eac5a1ee45c50c783e728a4af
Brett Smith <bsmith@mcad.edu>
2022-04-12 00:21:03 -0500

    oh god

--- week11/assign/sketchTwo.js
+++ week11/assign/sketchTwo.js
@@ -3,24 +3,37 @@ createCanvas(1000, 1000)
 }
     
 function draw(){
-    var diamater = 10
-    var width = diamater;
-    var height = diamater;
-    
+    r = 10;
+    makeDot = new Dot;
     for (let x = 0; x < 49; x++) {
         for (let y = 0; y < 49; y++) {
-            ellipse(20+ x * 20, 20+ y * 20, height, width); 
+            makedot[x];
         }
     }
-    function mouseOver() {
-        let d = dist(mouseX, mouseY, x,y);
-        if (d < 5) {
-        swell();}
-    }
-    function swell() {
-        diamater = diamater + random(1.5, 5);
-        time(30);
+}
+
+function mouseOver() {
+        let d = dist(mouseX, mouseY, this.x, this.y);
+        if (d < this.r) {
+             r = r + random(1.5, 5);}
     }
 
-    }    
+    
 
+class Dot {
+    constructor(x,y){
+    this.x = x;
+    this.y = y;
+    this.r = r;
+}
+    show() {
+    ellipse(this.x , this.y, this.r * 2);
+    }
+}
+    // function swell() {
+    //     diamater = diamater + random(1.5, 5);
+    //     time(30);
+    // }
+
+
+   
