aff984531bbdc2da66bbffc000fb151a101a1686
Brett Smith <bsmith@mcad.edu>
2022-04-11 23:55:11 -0500

    grid bump

--- week11/assign/sketchTwo.js
+++ week11/assign/sketchTwo.js
@@ -3,15 +3,24 @@ createCanvas(1000, 1000)
 }
     
 function draw(){
-
-    var diamater = 10;
+    var diamater = 10
+    var width = diamater;
+    var height = diamater;
     
     for (let x = 0; x < 49; x++) {
         for (let y = 0; y < 49; y++) {
-            ellipse(20+ x * 20, 20+ y * 20, diamater, diamater); 
+            ellipse(20+ x * 20, 20+ y * 20, height, width); 
         }
     }
+    function mouseOver() {
+        let d = dist(mouseX, mouseY, x,y);
+        if (d < 5) {
+        swell();}
+    }
+    function swell() {
+        diamater = diamater + random(1.5, 5);
+        time(30);
+    }
 
-   
     }    
-}
+
