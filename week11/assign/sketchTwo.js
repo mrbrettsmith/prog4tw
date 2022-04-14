29d87493dcfc801d8260ba3c8521699498af2366
Brett Smith <bsmith@mcad.edu>
2022-04-13 20:15:11 -0500

    Update sketchTwo.js

--- week11/assign/sketchTwo.js
+++ week11/assign/sketchTwo.js
@@ -1,41 +1,96 @@
+let dots = [];
 
 function setup(){
-createCanvas(1000, 1000)
-}
-
-function draw() {
+    createCanvas(1000, 1000)
     background('black')
+    for (let i = 0; i < width; i++) {
+        dots[i];
+    }
     size = 15
     for (let r = size; r <= width - size; r += size * 2) {
         for (let d = size; d <= height - size; d += size * 2) {
-          push();
-          rectMode(CENTER);
-          translate(r, d);
-          noStroke();
-          fill(170, 8, 207);
-          
+            push();
+            translate(r, d);
+            noStroke();
+            fill(170, 8, 207);
+            this.x = r;
+            this.y = d;
+            this.rad = size;
+            ellipse(this.x , this.y, this.rad );
+            // Grow Function
+            function mouseClicked() {
+                for (let i = 0; i < i.length; i++) {
+                    let hov = dist(mouseX, mouseY, this.x, this.y);
+                        
+                    if (hov < this.rad) {
+                        console.log("THIS IS ", i);
+                        rad = rad + random(1.5, 5);
+                    }    
+                }
 
-          this.x = x;
-          this.y = y;
-            this.r = r;
-              
+            }
             
-            ellipse(this.x , this.y, this.r * 2);
-
-          swell;
-          pop();
+            
+            pop();
         }
-    }
-
+    }    
 }
 
-function swell (mouseX, mouseY) {
-        let hov = dist(mouseX, mouseY, this.x, this.y);
-        if (hov < this.r) {
-            console.log(d);
-            r = r + random(1.5, 5);
-        }    
+function draw() {
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].show;
     }
+}
+
+
+
+// function mouseClicked() {
+//         let hov = dist(mouseX, mouseY, this.x, this.y);
+        
+//         }
+//         for (let i = 0; i < dots.length; i++) {
+//         dots[i] = dots[i];
+//         if (hov < this.rad) {
+//             console.log("THIS IS ", i);
+//             rad = rad + random(1.5, 5);
+//     }
+// }
+
+
+
+// class
+
+    // size = 15
+    // for (let r = size; r <= width - size; r += size * 2) {
+    //     for (let d = size; d <= height - size; d += size * 2) {
+    //         push();
+    //         translate(r, d);
+    //         noStroke();
+    //         fill(170, 8, 207);
+    //         this.x = r;
+    //         this.y = d;
+    //         this.rad = size;
+    //         ellipse(this.x , this.y, this.rad );
+    //         // Grow Function
+    //         let hov = dist(mouseX, mouseY, this.x, this.y);
+    //         if (hov < this.rad) {
+    //             console.log(d);
+    //             rad = rad + random(1.5, 5);
+    //         }
+    //     //   swell;
+    //       pop();
+    //     }
+    // }
+
+// }
+
+// function swell (mouseX, mouseY) {
+//         let hov = dist(mouseX, mouseY, this.x, this.y);
+//         if (hov < this.r) {
+//             console.log(d);
+//             r = r + random(1.5, 5);
+//         }    
+//     }
 
 
 // GRAVEYARD OF PREVIOUS ATTEMPTS
