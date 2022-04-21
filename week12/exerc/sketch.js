ef236a4e80252b9c4e47db4f359db6286082e997
Brett Smith <bsmith@mcad.edu>
2022-04-20 20:31:35 -0500

    Fixed exercise 12

---
+++ week12/exerc/sketch.js
@@ -0,0 +1,29 @@
+let soundArray = [];
+var button;
+
+function preload() {
+    // soundFormats('mp3');
+    soundArray = [
+        loadSound('sample/compOne.mp3'),
+        loadSound('sample/compTwo.mp3'),
+        loadSound('sample/compThree.mp3'),
+        loadSound('sample/compFour.mp3'),
+    ]
+}
+
+function setup(){
+    createCanvas(600, 600);
+    button = createButton('Click to Compute');
+    button.mousePressed(randomFx);
+}
+
+function randomFx() {       
+    random(soundArray).play();
+}
+
+function draw(){
+background("black")
+fill('green')
+button.position(283,310);
+text('PROCESSING...', width/2, height/2);
+}
