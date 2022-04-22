2073d4f714d3a80a8e4ab1192b5d911d49e7b6ab
Brett Smith <bsmith@mcad.edu>
2022-04-21 19:19:51 -0500

    sunflower

--- week13/exerc/main.js
+++ week13/exerc/main.js
@@ -9,10 +9,31 @@ function setup(){
 }
 
 function draw(){
-clear();
-var mySignVal = sin(position);
-// console.log(mySignVal);
-ellipse(mySignVal*50 + 300, height/2 + (mySignVal *  100), mySignVal * 20, 50);
 
-position = position + inc;
+    clear();
+
+    var mySignVal = sin(position);
+    ampSin = mySignVal * 50;
+
+    translate(width/2, height/2);
+    
+
+    for (let x = 0; x<=360; x = x + 10) {
+        
+        // curve(0, 10, ampSin, 20, ampSin, 10, 10, 10);
+        triangle(-5,0, -ampSin, 300, ampSin, 300)
+        
+        
+        rotate(x);
+        
+    }
+
+
+    position = position + inc;
+
+
+
 }
+
+// console.log(mySignVal);
+    // ellipse(ampSin + 300, height/2 + (mySignVal *  100), mySignVal * 20, 50);
