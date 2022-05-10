c2a29344808fc2947ac164e4e4c2dd2c47fd4f5e
Brett Smith <bsmith@mcad.edu>
2022-05-10 17:15:53 -0500

    Time to Clean Up

--- Final/main.js
+++ Final/main.js
@@ -1,7 +1,7 @@
 var drawArray = [];
 var sigilArray1 = [];
 var sigilArray2 = [];
-var makeSigils3 = [];
+var sigilArray3 = [];
 var terminator;
 
 var transferX;
@@ -37,6 +37,11 @@ function draw() {
         sigilArray2[y].animate();
         sigilArray2[y].show();
     }
+    for (let y = 0; y < sigilArray3.length; y++) {
+        sigilArray3[y].animate();
+        sigilArray3[y].show();
+    }
+
 
 
     test = test + 1;
@@ -58,6 +63,17 @@ function mousePressed(){
         let makeSigils2 = new Sigil3(transferX,transferY,terminator);
         sigilArray2.push(makeSigils2);
     }
+    if (clickTally === 2){
+        var terminator = round(random(-100, 100),0);
+        let makeSigils3 = new Sigil4(transferX,transferY,terminator);
+        sigilArray3.push(makeSigils3);
+    }
+    if (clickTally === 3){
+        var terminator = round(random(-100, 100),0);
+        let makeSigils2 = new Sigil3(transferX,transferY,terminator);
+        sigilArray2.push(makeSigils2);
+    }
+
 
     clickTally = clickTally + 1;
     console.log('clickTally', clickTally) 
@@ -140,6 +156,76 @@ function Sigil3(x,y,terminator) {
     this.beginY = y;
 
     this.endX = x + 150 + terminator;
+    this.endY = y + 150 + terminator;
+
+    this.terminator = terminator;
+
+    // for percentage of distance traveled //
+    let step = 0.01;
+    let pct = 0.0;
+
+
+    // storing draw path //
+    this.history = [];
+    
+
+
+    // let v0 = createVector(x,y);
+    // let v1 = (x, y + 100 + terminator)
+
+
+    rectMode(CENTER)
+    
+    // animate as update //
+
+    this.animate = function() {
+        
+
+        this.distX = this.endX - this.beginX;
+        this.distY = this.endY - this.beginY;
+    }
+
+    this.finished = function() {
+        return pct === 1.0;
+    }
+
+    this.show = function() {
+        noStroke();
+        fill("red");
+        rect(this.goX, this.goY, 10, 10);
+        
+        var v = createVector(this.goX, this.goY)
+        this.history.push(v);
+        
+        beginShape();
+        pct += step;
+        if (pct < 1.0) {
+
+            this.goX = round(this.beginX + pct * this.distX);
+            
+            this.goY = round(this.beginY + pct * this.distY);
+        } 
+
+        // Drawing History //
+        for (var i = 1; i < this.history.length; i++) {
+
+            var position = this.history[i];
+            fill("red");
+            rect(position.x, position.y, 10, 10); 
+        }
+        endShape();
+
+
+    }
+    transferX = this.endX;
+    transferY = this.endY;
+}
+function Sigil4(x,y,terminator) {
+    
+    this.beginX = x;
+    this.beginY = y;
+
+    this.endX = x - 150 + terminator;
     this.endY = y + 150 + terminator;
 
     this.terminator = terminator;
